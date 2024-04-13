41f2aa115c844bec4beb7f5bb26b0e757f5cbc1b
SamyNandes <samyrafernandes89@gmail.com>
2024-04-13 20:59:38 -0300

    Nova Atualizacao

--- script.js
+++ script.js
@@ -6,54 +6,57 @@ function adicionar(n){
     if(typeof n === "number" || operando != ""){
         valor += n.toString()
         document.getElementById("tela").innerHTML = valor2 + operando + valor + resultado
-        console.log(valor)
-        console.log(valor2)
     } 
     switch(n){
         case "+":
         operando = "+"
         valor2 = valor
         valor = ""
+        document.getElementById("tela").innerHTML = valor2 + operando + valor + resultado
         break;
         case "-": 
         operando = "-"
         valor2 = valor
         valor = ""
+        document.getElementById("tela").innerHTML = valor2 + operando + valor + resultado
         break;
         case "*": 
         operando = "*"
         valor2 = valor
         valor = ""
+        document.getElementById("tela").innerHTML = valor2 + operando + valor + resultado
         break;
         case "/": 
         operando = "/"
         valor2 = valor
         valor = ""
+        document.getElementById("tela").innerHTML = valor2 + operando + valor + resultado
         break;
     }
     if(n === '='){
         switch(operando){
         case "+":
                 resultado = parseFloat(valor2) + parseFloat(valor)
-                document.getElementById("tela").innerHTML = valor2 + operando + valor + resultado
-            break;
+                break;
             case "-": 
                 resultado = parseFloat(valor2) - parseFloat(valor)
-                document.getElementById("tela").innerHTML = valor2 + operando + valor + resultado
-            break;
+                break;
             case "*": 
                 resultado = parseFloat(valor2) * parseFloat(valor)
-                document.getElementById("tela").innerHTML = valor2 + operando + valor + resultado
             break;
             case "/": 
                 if (parseFloat(valor) === 0){
                     novoCalculo()
-                    document.getElementById("tela").innerHTML = "erro"
                 } else {
                     resultado = parseFloat(valor2) / parseFloat(valor)
                     document.getElementById("tela").innerHTML = valor2 + operando + valor + resultado
-                }    
-            break;
+                } break;
+        }
+        if (isNaN(parseFloat(valor2)) || isNaN(parseFloat(valor)) || isNaN(resultado)  == true){
+            novoCalculo()
+            document.getElementById("tela").innerHTML = "Erro, faça um novo cálculo"
+        } else {  
+        document.getElementById("tela").innerHTML = valor2 + operando + valor + resultado    
         }
     }
 }
